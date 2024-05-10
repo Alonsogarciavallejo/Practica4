41f81092931e01a7259bc2224759eba835db9ae0
Alonsogarciavallejo <115785376+Alonsogarciavallejo@users.noreply.github.com>
2024-05-10 09:47:19 +0200

    Update principal.java

--- Practica 4/Aplicacion/principal.java
+++ Practica 4/Aplicacion/principal.java
@@ -1,9 +1,9 @@
 
 public class principal {
+       // Ejemplo de uso
     public static void main(String[] args) {
-        String s1 = "casa";
-        String s2 = "calle";
-        System.out.println("La distancia de edición entre \"" + s1 + "\" y \"" + s2 + "\" es: " +
-                CalculadorDistancias.CalcularDistancias(s1, s2));
+        String palabra1 = "rafa";
+        String palabra2 = "paloma";
+        System.out.println("La distancia de edición entre '" + palabra1 + "' y '" + palabra2 + "' es: " + calcularDistancia(palabra1, palabra2));
     }
 }
